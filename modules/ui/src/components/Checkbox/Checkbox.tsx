1d426ecb205c7bf137f642a5de573049b81bc0b2
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-10-17 19:54:53 +0500

    Fix: Checkbox
    + Исправлено отображение галочки чекбокса

--- modules/ui/src/components/Checkbox/Checkbox.tsx
+++ modules/ui/src/components/Checkbox/Checkbox.tsx
@@ -9,16 +9,15 @@ export const Checkbox = observer(
   forwardRef<HTMLInputElement, CheckboxProps>(
     ({ className, ...props }, ref) => {
       return (
-        <label className={cn('flex size-5', className)}>
+        <label className={cn('flex size-5 relative', className)}>
           <input
             ref={ref}
             className={`peer hidden`}
             type="checkbox"
             {...props}
           />
-          <span className="border-gray-[#d9d9d9] hover:border-primary peer-checked:border-primary peer-checked:bg-primary flex size-full items-center justify-center rounded-sm border-2 transition-colors">
-            <CheckSvg className="not:peer-checked:hidden" />
-          </span>
+          <span className="border-gray-[#d9d9d9] hover:border-primary peer-checked:border-primary peer-checked:bg-primary flex size-full items-center justify-center rounded-sm border-2 transition-colors"></span>
+          <CheckSvg className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 transition-opacity peer-checked:opacity-100" />
         </label>
       );
     }
