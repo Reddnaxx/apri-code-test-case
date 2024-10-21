58352945679b4a9dfe1909076f1e75e01783ff40
Александр Рукавишников <as.rukavishnikov@mail.ru>
2024-10-21 13:53:36 +0500

    Mobile adaptive
    + Добавлена адаптивность для мобильных устройств

--- src/routes/Layout/Layout.tsx
+++ src/routes/Layout/Layout.tsx
@@ -7,8 +7,8 @@ import Providers from '../Providers';
 export const Layout = observer(() => {
   return (
     <Providers>
-      <div className="mx-auto flex min-h-screen max-w-screen-xl">
-        <TaskList className="flex-1 pr-9 pt-9" />
+      <div className="mx-auto flex min-h-dvh flex-col pt-4 md:max-w-screen-xl md:flex-row md:p-0">
+        <TaskList className="flex-1 px-4 md:pr-9 md:pt-9" />
         <main className="flex-1 bg-[#DCE0E1] dark:bg-zinc-800">
           <Outlet />
         </main>
